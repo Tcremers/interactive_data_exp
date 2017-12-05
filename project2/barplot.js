caaebb1010cf2dacb4c50d03b9a8d54cbfc17653
Tcremers <tychoow@live.nl>
2017-12-05 16:20:30 +0100

    Completed project 2

---
+++ project2/barplot.js
@@ -0,0 +1,52 @@
+d3.select(window).on('load', init);
+
+function init() {
+	d3.csv(
+	'temperaturesKobenhavn.csv',
+	function(d) {
+	  return {
+		year : +d.YEAR,
+		mean : +d.metANN
+	  };
+	},
+	function(error, data) {
+		if (error) throw error;
+		var svgOne = d3.select('body')
+					.select('svg');
+		var margin = {top: 10, right: 10, bottom: 10, left: 50};
+		var width = 1200
+		var height = 500
+		var gOne = svgOne.append("g")
+		  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+		var x = d3.scaleBand().rangeRound([0, width]).padding(0.3)
+				.domain(data.map(function(d) { if(d.year % 2 == 0){return d.year; } }));
+
+		var y = d3.scaleLinear().rangeRound([height, 0])
+				.domain([0, d3.max(data, function(d) { return d.mean; })]);
+
+		gOne.append("g")
+		.attr("class", "axis x")
+		.attr("transform", "translate(0," + height + ")")
+		.call(d3.axisBottom(x))
+		.selectAll("text")
+			.attr("y", 0)
+			.attr("x", 9)
+			.attr("dy", ".35em")
+			.attr("transform", "rotate(90)")
+			.style("text-anchor", "start");
+
+
+		gOne.append("g")
+		  .attr("class", "axis y")
+		  .call(d3.axisLeft(y).ticks(10));
+
+		gOne.selectAll(".bar")
+		  .data(data)
+		  .enter().append("rect")
+		  .attr("x", function(d) { return x(d.year); })
+		  .attr("y", function(d) { return y(d.mean); })
+		  .attr("width", x.bandwidth())
+		  .attr("height", function(d) { return height - y(d.mean); });
+	});
+}
---
+++ project2/heatmap.js
@@ -0,0 +1,73 @@
+var data;
+var matrix_data = [];
+
+$.get('temperature_copenhagen.txt', function(d){
+	data = d;
+	
+	
+	
+	data = data.split(/\r?\n/);
+	data_header = data[0].split(/[ ]+/);
+	data_header.shift(); 
+	data.shift();
+	
+	for(var i = 0; i < data.length; i++){
+		var line = data[i].split(/[ ]+/);
+		//data_header.push(line[0]);
+		line.shift();
+		matrix_data.push(line);
+	}
+	
+	
+	// Flatten matrix
+	var m = matrix_data.length;
+	var n = matrix_data[0].length;
+	flat_data = [].concat.apply([], matrix_data);
+
+
+	// Largest value in data. Used to scale colors.
+	var maxval = d3.max(flat_data);
+
+	// Initialiation function. Called after body has loaded
+	var w = 1000;
+	var h = 1000;
+	var svgTwo = d3.select('body')
+			  .append('svg')
+			  .attr('width', w)
+			  .attr('height', h);
+	
+	var sqW = w/n;
+	var sqH = h/m;
+	var x = d3.scaleBand().rangeRound([10, w]).domain(data_header.map(function(d) { return d; }));
+	
+	// Create rects for entry and color them according to value
+	
+	svgTwo.append("g")
+		.attr("class", "axis axis--x")
+		//.attr("transform", "translate(0," + h + ")")
+		.call(d3.axisBottom(x));
+
+			
+	svgTwo.selectAll('rect')
+	  .data(flat_data)  
+	  .enter()
+	  .append('rect')
+	  .attr('x',function(d,i){
+		var col = i%n;
+		var offset = 10;
+		return col*sqW+offset;
+	  })
+	  .attr('y',function(d,i){
+		var row = Math.floor(i/n);
+		var offset = 20;
+		return row*sqH+offset;
+	  })
+	  .attr('width' , w/n+1)
+	  .attr('height', h/m+1)
+	  .style('fill',function(d){
+		var I = Math.round(120*(1 - d/maxval)+150);
+		return 'rgb('+I+','+(I-100)+','+(I-100)+')'; 
+	  });
+			  
+});
+		
