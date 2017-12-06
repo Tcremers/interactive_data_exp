9b83f7b72d26df513ec6a6c87de8cb4f8372c278
Tycho <tychoow@live.nl>
2017-12-06 16:01:40 +0100

    Added y axis to heat map

--- project2/heatmap.js
+++ project2/heatmap.js
@@ -1,19 +1,19 @@
 var data;
 var matrix_data = [];
-
+var row_names = [];
 $.get('temperature_copenhagen.txt', function(d){
 	data = d;
 	
 	
 	
 	data = data.split(/\r?\n/);
-	data_header = data[0].split(/[ ]+/);
-	data_header.shift(); 
+	var column_names = data[0].split(/[ ]+/);
+	column_names.shift(); 
 	data.shift();
 	
 	for(var i = 0; i < data.length; i++){
 		var line = data[i].split(/[ ]+/);
-		//data_header.push(line[0]);
+		row_names.push(line[0]);
 		line.shift();
 		matrix_data.push(line);
 	}
@@ -34,28 +34,48 @@ $.get('temperature_copenhagen.txt', function(d){
 	var svgTwo = d3.select('body')
 			  .select('#Results')
 			  .append('svg')
-			  .attr('width', w)
+			  .attr('width', w+100)
 			  .attr('height', h);
 	
 	var sqW = w/n;
 	var sqH = h/m;
-	var x = d3.scaleBand().rangeRound([10, w]).domain(data_header.map(function(d) { return d; }));
 	
+	var x = d3.scaleBand().rangeRound([30, w+20]).domain(column_names.map(function(d) { return d; }));
+	
+	//var y = d3.scaleBand().rangeRound([0, h]).domain(row_names.map(function(d){return String(d);}));
 	// Create rects for entry and color them according to value
 	
 	svgTwo.append("g")
 		.attr("class", "axis axis--x")
 		//.attr("transform", "translate(0," + h + ")")
 		.call(d3.axisBottom(x));
-
-			
+		
+	//svgTwo.append("g")
+	//	  .attr("class", "axis y")
+		//  .call(d3.axisLeft(y))
+		  //.attr("transform", "translate(" + 30 + "," + 0 + ")");
+	
+	svgTwo.append('g')
+	  .selectAll('text')
+	  .data(row_names)
+	  .enter()
+	  .append('text')
+	  .attr('x',0)
+	  .attr('y',function(d,i){
+		var row = Math.floor((i*n)/n);		
+		var offset = 20;
+		return row*sqH+offset+((h/m));
+	  })
+	  .text(function(d){return d})
+	  .style("font-size","8px");
+	
 	svgTwo.selectAll('rect')
 	  .data(flat_data)  
 	  .enter()
 	  .append('rect')
 	  .attr('x',function(d,i){
 		var col = i%n;
-		var offset = 10;
+		var offset = 30;
 		return col*sqW+offset;
 	  })
 	  .attr('y',function(d,i){
@@ -67,8 +87,9 @@ $.get('temperature_copenhagen.txt', function(d){
 	  .attr('height', h/m+1)
 	  .style('fill',function(d){
 		var I = Math.round(120*(1 - d/maxval)+150);
-		return 'rgb('+I+','+(I-100)+','+(I-100)+')'; 
+		return 'rgb('+I+','+(I-100)+','+(I-100)+')';
 	  });
-			  
+	  
+	
 });
 		
